2bf8fdef2c8716512d321441a68745b0ee4ef5ed
Francelp <francel_piedra@hotmail.com>
2015-11-25 10:32:10 +0000

    Francel

---
+++ ToDoListProject/ToDoListProject/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListProject
+{
+    public class ViewModel
+    {
+        private List<string> _items;
+        public List<string> items
+        {
+            get { return _item; }
+            set { _item = value; }
+        }
+
+        public ViewModel()
+        {
+        }
+
+
+    }
+}
