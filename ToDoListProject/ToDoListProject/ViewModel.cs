a425f9b2edc9e03ecd0de4711972e9bf4441aab2
Psela <gaelbouyer@fdmgroup.com>
2015-11-25 12:02:07 +0000

    Mainview selected

--- ToDoListProject/ToDoListProject/ViewModel.cs
+++ ToDoListProject/ToDoListProject/ViewModel.cs
@@ -9,114 +9,138 @@ using System.Windows.Input;
 
 namespace ToDoListProject
 {
-    public class ViewModel : INotifyPropertyChanged
+  public class ViewModel : INotifyPropertyChanged
+  {
+    private ObservableCollection<ToDoItem> _toDoItem;
+    public ObservableCollection<ToDoItem> toDoItem
     {
-        private ObservableCollection<ToDoItem> _toDoItem;
-        public ObservableCollection<ToDoItem> toDoItem
-        {
-            get { return _toDoItem; }
-            set { _toDoItem = value;
-            onPropertyChanged("toDoItem");
-            }
-        }
-        
-        private ICommand _addToList;
-        public ICommand addToList
-        {
-            get {
-                if (_addToList == null)
-                {
-                    _addToList = new Command(AddToList, CanAddToList);
-                }
-                return _addToList; }
-            set { _addToList = value; }
-        }
+      get { return _toDoItem; }
+      set
+      {
+        _toDoItem = value;
+        onPropertyChanged("toDoItem");
+      }
+    }
 
-        private ICommand _removeFromList;
-        public ICommand removeFromList
+    private ToDoItem _selectedItemToDo;
+    public ToDoItem selectedItemToDo
+    {
+      get { return _selectedItemToDo; }
+      set
+      {
+        _selectedItemToDo = value;
+        onPropertyChanged("selectedItemToDo");
+      }
+    }
+
+
+    private ICommand _addToList;
+    public ICommand addToList
+    {
+      get
+      {
+        if (_addToList == null)
         {
-            get {
-                if (_removeFromList == null)
-                {
-                    _removeFromList = new Command(RemoveFromList, CanRemoveFromList);
-                }
-                return _removeFromList; }
-            set { _removeFromList = value; }
+          _addToList = new Command(AddToList, CanAddToList);
         }
+        return _addToList;
+      }
+      set { _addToList = value; }
+    }
 
-        private ICommand _editList;
-        public ICommand editList
+    private ICommand _removeFromList;
+    public ICommand removeFromList
+    {
+      get
+      {
+        if (_removeFromList == null)
         {
-            get {
-                if (_editList == null)
-                {
-                    _editList = new Command(EditList, CanEditList);
-                }
-                return _editList; }
-            set { _editList = value; }
+          _removeFromList = new Command(RemoveFromList, CanRemoveFromList);
         }
+        return _removeFromList;
+      }
+      set { _removeFromList = value; }
+    }
 
-        private ICommand _complete;
-        public ICommand complete
+    private ICommand _editList;
+    public ICommand editList
+    {
+      get
+      {
+        if (_editList == null)
         {
-            get {
-                if (_complete == null)
-                {
-                    _complete = new Command(Complete,CanComplete);
-                }
-                return _complete; }
-          set { _complete = value; }
+          _editList = new Command(EditList, CanEditList);
         }
+        return _editList;
+      }
+      set { _editList = value; }
+    }
 
-        private bool CanComplete()
+    private ICommand _complete;
+    public ICommand complete
+    {
+      get
+      {
+        if (_complete == null)
         {
-          return true;
+          _complete = new Command(Complete, CanComplete);
         }
+        return _complete;
+      }
+      set { _complete = value; }
+    }
 
-        private void Complete()
-        {
+    private bool CanComplete()
+    {
+      return true;
+    }
 
-        }
+    private void Complete()
+    {
 
-        private void AddToList()
-        {
-            toDoItem = new ObservableCollection<ToDoItem>()
+    }
+
+    private void AddToList()
+    {
+      toDoItem = new ObservableCollection<ToDoItem>()
             {
                 new ToDoItem(1,"Shopping", false),
                 new ToDoItem(2,"Ironing", false),
                 new ToDoItem(3, "Washing", true)
             };
-        }
-        private bool CanAddToList()
-        {
-            return true;
-        }
 
-        private void RemoveFromList()
-        {
-        }
-        private bool CanRemoveFromList()
-        {
-            return true;
-        }
-        
-        private void EditList()
-        {
-
-        }
-        private bool CanEditList()
-        {
-            return true;
-        }
-        
-        
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void onPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+      selectedItemToDo = toDoItem[0];
     }
+    private bool CanAddToList()
+    {
+      return true;
+    }
+
+    private void RemoveFromList()
+    {
+    }
+    private bool CanRemoveFromList()
+    {
+      return true;
+    }
+
+    private void EditList()
+    {
+
+    }
+    private bool CanEditList()
+    {
+      return true;
+    }
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void onPropertyChanged(string propertyName)
+    {
+      if (PropertyChanged != null)
+      {
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
+    }
+  }
 }
